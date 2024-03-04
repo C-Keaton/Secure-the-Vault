eb37f62fc8b848dba8410bf37f1d55966f77905c
CoriganK <corigankeaton@gmail.com>
2024-03-04 16:01:40 -0500

    Added Vault Codes

---
+++ scriptfile.js
@@ -0,0 +1,17 @@
+/* Psudocode
+const secretNum1 = 5 + 5;
+const secretNum2 = secretNum1 * 4;
+const secretNum3 = secretNum2 - 1;
+
+console.log(secretNum1);
+console.log(secretNum2);
+console.log(secretNum3); */
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39");
+
+//secretNum1 is addition, secretNum2 is multiplication, and secretNum3 is subtraction
+const secretNum1 = 5 + 5;
+const secretNum2 = 20 * 2;
+const secretNum3 = 44 - 5;
+
+alert(`The Vault Code is: ${secretNum1}, ${secretNum2}, and ${secretNum3}`)
